617eccf02b0f15ca2fc39f6cf395257b7f675ec6
Philip Jackson <p-jackson@live.com>
2025-12-10 00:19:08 +1300

    day 5 part 2 complete

--- day5/main.zig
+++ day5/main.zig
@@ -1,12 +1,30 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
 const Range = struct {
     lower: u64,
     higher: u64,
 
-    pub fn in_range(self: *const Range, n: u64) bool {
-        return n >= self.lower and n <= self.higher;
+    pub fn print(self: *const Range) void {
+        std.debug.assert(self.lower <= self.higher);
+        std.debug.print("{d}-{d}\n", .{ self.lower, self.higher });
+    }
+
+    pub fn intersects(self: *const Range, other: *const Range) bool {
+        return (other.lower <= self.higher and other.lower >= self.lower) or
+            (other.higher >= self.lower and other.higher <= self.higher) or
+            (self.higher >= other.lower and self.higher <= other.higher) or
+            (self.lower <= other.higher and self.lower >= other.lower);
+    }
+
+    pub fn merge(self: *Range, other: *const Range) void {
+        self.lower = @min(self.lower, other.lower);
+        self.higher = @max(self.higher, other.higher);
+    }
+
+    pub fn count(self: *const Range) u64 {
+        return self.higher - self.lower + 1;
     }
 };
 
@@ -35,18 +53,52 @@ pub fn main() !void {
         try ranges.append(alloc, Range{ .lower = lower, .higher = higher });
     }
 
-    var count: u64 = 0;
+    var done = false;
 
-    while (try reader.takeDelimiter('\n')) |current_line| {
-        const n = try std.fmt.parseInt(u64, current_line, 10);
+    blk: while (!done) {
+        done = true;
 
-        for (ranges.items) |range| {
-            if (range.in_range(n)) {
-                count += 1;
-                break;
+        for (0..ranges.items.len) |i| {
+            const candidate = ranges.items[i];
+
+            for (i + 1..ranges.items.len) |j| {
+                if (ranges.items[j].intersects(&candidate)) {
+                    done = false;
+                    ranges.items[j].merge(&candidate);
+                    _ = ranges.orderedRemove(i);
+                    continue :blk;
+                }
             }
         }
     }
 
+    var count: u64 = 0;
+
+    for (ranges.items) |range| count += range.count();
+
     std.debug.print("{d}\n", .{count});
+}
+
+test "intersection" {
+    try expect(!(Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 120, .higher = 1100 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 100, .higher = 110 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 90, .higher = 110 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 20, .higher = 80 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 20, .higher = 100 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 10, .higher = 80 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 5, .higher = 80 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 5, .higher = 10 }));
+    try expect(!(Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 5, .higher = 6 }));
+
+    try expect(!(Range{ .lower = 120, .higher = 1100 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 100, .higher = 110 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 90, .higher = 110 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 10, .higher = 100 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 20, .higher = 80 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 20, .higher = 100 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 10, .higher = 80 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 5, .higher = 80 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect((Range{ .lower = 5, .higher = 10 }).intersects(&Range{ .lower = 10, .higher = 100 }));
+    try expect(!(Range{ .lower = 5, .higher = 6 }).intersects(&Range{ .lower = 10, .higher = 100 }));
 }
