07c7a20d9e69bedbf5b227f96ab2f1718adb5647
Philip Jackson <p-jackson@live.com>
2025-12-12 22:55:00 +1300

    day 6 part 2 complete

--- day6/main.zig
+++ day6/main.zig
@@ -10,44 +10,67 @@ pub fn main() !void {
     var file_reader = file.reader(&io_buffer);
     var reader = &file_reader.interface;
 
-    var columns: std.ArrayList(std.ArrayList(u64)) = .empty;
-    defer columns.deinit(alloc);
-    defer for (columns.items) |*c| c.deinit(alloc);
+    var all_lines: std.ArrayList([]u8) = .empty;
+    defer all_lines.deinit(alloc);
+    defer for (all_lines.items) |c| alloc.free(c);
 
     var grand_total: u64 = 0;
 
     while (try reader.takeDelimiter('\n')) |current_line| {
-        if (current_line.len == 0) {
-            break;
+        if (current_line.len == 0) break;
+        try all_lines.append(alloc, try alloc.alloc(u8, current_line.len));
+        @memcpy(all_lines.items[all_lines.items.len - 1], current_line);
+    }
+
+    const lines = all_lines.items[0 .. all_lines.items.len - 1];
+    const operations = all_lines.items[all_lines.items.len - 1];
+
+    var last_operator_idx: usize = 0;
+    for (1..operations.len) |i| {
+        const next_operator = operations[i];
+        if (next_operator != '*' and next_operator != '+' and i != operations.len - 1) {
+            continue;
         }
 
-        var iter = std.mem.splitAny(u8, current_line, " ");
+        const num_end_idx = if (i == operations.len - 1) i + 1 else i - 1;
 
-        var col_index: usize = 0;
-        while (iter.next()) |num| {
-            if (num.len == 0) continue;
+        grand_total += process(lines, last_operator_idx, num_end_idx, operations[last_operator_idx]);
 
-            if (col_index <= columns.items.len) {
-                try columns.append(alloc, .empty);
-            }
-
-            const n = std.fmt.parseInt(u64, num, 10) catch {
-                if (num[0] == '+') {
-                    for (columns.items[col_index].items) |a| grand_total += a;
-                }
-                if (num[0] == '*') {
-                    var product: u64 = 1;
-                    for (columns.items[col_index].items) |a| product *= a;
-                    grand_total += product;
-                }
-                col_index += 1;
-                continue;
-            };
-
-            try columns.items[col_index].append(alloc, n);
-            col_index += 1;
-        }
+        last_operator_idx = i;
     }
 
     std.debug.print("{d}\n", .{grand_total});
+}
+
+fn process(lines: [][]u8, begin: usize, end: usize, operation: u8) u64 {
+    var accumulator: u64 = switch (operation) {
+        '+' => 0,
+        '*' => 1,
+        else => unreachable,
+    };
+
+    var str_buff: [128]u8 = undefined;
+    var str_len: usize = 0;
+    std.debug.assert(lines.len < str_buff.len);
+
+    for (begin..end) |i| {
+        str_len = 0;
+
+        for (lines) |line| {
+            if (line[i] != ' ') {
+                str_buff[str_len] = line[i];
+                str_len += 1;
+            }
+        }
+
+        const n = std.fmt.parseInt(u64, str_buff[0..str_len], 10) catch unreachable;
+
+        switch (operation) {
+            '+' => accumulator += n,
+            '*' => accumulator *= n,
+            else => unreachable,
+        }
+    }
+
+    return accumulator;
 }
